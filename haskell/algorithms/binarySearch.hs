7fda0e3f217548b9786b9d03c51aa349e26707fe
yattew <yathansh@gmail.com>
2022-04-16 20:08:49 +0530

    binary search in haskell better syntax

--- haskell/algorithms/binarySearch.hs
+++ haskell/algorithms/binarySearch.hs
@@ -1,18 +1,13 @@
 bSearchHelper :: [Int] -> Int -> Int -> Int -> Int
 bSearchHelper ls x low high
-    | low > high = -1
-    | otherwise = 
-        if el == x 
-                then mid
-            else 
-                if el > x 
-                    then bSearchHelper ls x low $ mid - 1
-                    else bSearchHelper ls x (mid + 1) high
-        where 
-            mid = low + (high - low) `div` 2
-            el = ls !! mid
+  | low > high = -1
+  | el == x = mid
+  | el > x = bSearchHelper ls x low (mid -1)
+  | el < x = bSearchHelper ls x (mid + 1) high
+  | otherwise = -1
+  where
+    mid = low + (high - low) `div` 2
+    el = ls !! mid
 
-binarySearch :: [Int] -> Int->Int
-binarySearch ls el = bSearchHelper ls el 0 (length ls -1)   
-
-
+binarySearch :: [Int] -> Int -> Int
+binarySearch ls el = bSearchHelper ls el 0 (length ls - 1)
